10f57fe425b308e7d16ccffcdc7d94838d98a30b
Shon <shonpatel07@gmail.com>
2024-04-06 18:34:30 +0530

    Ok

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+addition()
+{
+	int a,b;
+	cout<<"Enter two numbers";
+	cin>>a>>b;
+	cout<<a+b;
+}
+int main()
+{
+	addition();
+	cout<<"Addition";
+	return 0;
+	
+}
